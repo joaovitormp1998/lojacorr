7615791eb68886273031fcac3a771de1239ce54e
João Vitor Monsores <joaovitormonsores@hotmail.com>
2024-07-12 11:09:00 -0300

    add models and migrations category and subcategory

---
+++ desafio-lojacorr/app/Models/Subcategory.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Subcategory extends Model
+{
+    protected $fillable = ['name', 'category_id'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
